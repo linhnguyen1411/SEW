15c909078771f4f63185e287c2a7f8f1f84b9ec4
linhnguyen1411 <linhnguyencmu@gmail.com>
2017-11-16 00:19:38 +0700

    rebuilding home pages

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,5 +1,9 @@
 class News < ApplicationRecord
 	belongs_to :rss_url
-  default_scope { order(pubDate: :desc) }
-  scope :get_five_news, -> id {where "id < ?",id}
+  belongs_to :category
+  # default_scope { order(pubDate: :desc) }
+  scope :ordered_by_date, -> { order(pubDate: :desc) }
+  scope :get_news, -> id {where "id < ?",id}
+  scope :get_news_by_category, -> id{where category_id: id}
+  scope :get_news_by_cate_group, ->list {where "category_id IN ?",list.to_a}
 end
